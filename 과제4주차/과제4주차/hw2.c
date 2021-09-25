33c5da12222361debc9c2f6d4c72b46959734daf
Jiyun82 <202110918@sangmyung.kr>
2021-09-25 17:32:41 +0900

    Add File [과제4주차]

---
+++ 과제4주차/과제4주차/hw2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	float km, mile;
+
+	printf("Please enter kilometers: ");
+	scanf("%f", &km);
+
+	mile = km / 1.609;
+
+	printf("%.1f km is equal to %.1f miles.", km, mile);
+
+	return 0;
+}
